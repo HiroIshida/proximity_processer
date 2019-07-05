a49a9bceeff3785003c9f07d65825727d16c0e20
HiroIshida <spitfire.docomo@gmail.com>
2019-07-05 11:06:02 +0900

    append service

--- script/processer.py
+++ script/processer.py
@@ -8,26 +8,32 @@ class ProxProc:
     def __init__(self):
         sub = rospy.Subscriber("/proximity_sensor", ProximityStamped, self.callback_prox)
         survice_init = rospy.Service("init", Empty, self.handle_request_init)
+        survice_append = rospy.Service("append", Empty, self.handle_request_append)
         self.val = None
         self.val_ref = None
+        self.val_relative_lst = None
 
     def handle_request_init(self, req):
         log_str = "proximity processer init " + \
                 "with val:" + str(self.val)
         rospy.loginfo(log_str)
-        self.val_ref = self.val
+        self._init_procedure()
+        return EmptyResponse()
+
+    def handle_request_append(self, req):
+        val_relative = self.val -self.val_ref
+        log_str = "proximity processer append " + \
+                "with val:" + str(val_relative)
+        rospy.loginfo(log_str)
+        self.val_relative_lst.append(val_relative)
         return EmptyResponse()
 
     def callback_prox(self, data):
         self.val = data.proximity.average
 
-        ref_unset = (self.val_ref is None)
-        """
-        if ref_unset:
-            print "val_ref is not set yet"
-        else:
-            print self.val - self.val_ref
-            """
+    def _init_procedure(self):
+        self.val_ref = self.val
+        self.val_relative_lst = [0.0]
 
 if __name__=='__main__':
     rospy.init_node('proxproc', anonymous = True)
--- script/test.py
+++ script/test.py
@@ -7,9 +7,16 @@ def pp_init():
     client = rospy.ServiceProxy('init', Empty)
     client()
 
+def pp_append():
+    rospy.wait_for_service('append')
+    client = rospy.ServiceProxy('append', Empty)
+    client()
+
+
 if __name__=='__main__':
     rospy.init_node('tester_pp')
     pp_init()
+    pp_append()
 
     
 
